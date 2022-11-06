af7d8574198df1e98ada016596c7548facc2fd2f
Tolotra <tolotor@yahoo.fr>
2022-11-06 08:40:09 +0300

    Uploads paths config

---
+++ back/config/uploads-config.js
@@ -0,0 +1,9 @@
+const USERS_PATH = "/static/users/";
+const PROJECTS_PATH = "/static/projects/";
+const SKILLS_PATH = "/static/skills/";
+
+module.exports = {
+  USERS_PATH,
+  PROJECTS_PATH,
+  SKILLS_PATH,
+};
